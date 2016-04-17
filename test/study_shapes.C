1a35ba5f89f67afcf4ed5c18bcd841db122f99f2
degrutto <michele.de.gruttola@cern.ch>
2016-04-17 20:41:29 +0200

    shapes for fit

--- test/study_shapes.C
+++ test/study_shapes.C
@@ -4,13 +4,10 @@
 
   //  RooRealVar x("x","x", 594,1506);
   RooRealVar x("x","x", 518,1506);
-  //  x.setRange(520,1500);
 
-  TFile* fs = TFile::Open("TTsigLumi.root", "READ");
+    TFile* fs = TFile::Open("TTsigLumi.root", "READ");
 
-  // KEY: TH1Fsignal_BTag_TT_MassFSR;1M750: BTag_TT_MassFSR
-  //				   KEY: TH1Fbackground_BTag_TT_MassFSR;1HT100to200: BTag_TT_MassFSR
-
+  
 
   TH1F* mass_s = (TH1F*)fs->Get("signal_BTag_TT_MassFSR");
   RooDataHist data_s("data_s", "data signal", x, mass_s);
@@ -27,7 +24,6 @@
 
   //  w.factory("RooCBShape:sig_pdf(x[110.,150.], mean[125.,120.,130.], sigma[3.,1.,5.], alpha[-5.,-30.,0.],n[0.1,0.,1000.])");
 
-  //  RooRealVar xp("xp", "xp", 200,2000);
   RooRealVar mean("mean", "mean", 710, 500, 900);
   RooRealVar sigma("sigma", "sigma", 60, 0,100);
   RooRealVar alpha("alpha", "alpha", -2, -10,10);
@@ -87,42 +83,12 @@
   w->import(ch_b);
   w->import(data_b);
   w->import(data_s);
-  //  w->import(SUM:model(buk_s,ch_b));  
-
- 
-  // Print workspace contents
+    // Print workspace contents
   w->Print() ;
 
-  /*
-  RooAbsPdf * pdf = w.pdf("model");
-
-  RooStats::ModelConfig mc("ModelConfig",&w);
-  mc.SetPdf(*pdf);
-  mc.SetParametersOfInterest(*w.var("nsig"));
-  mc.SetObservables(*w.var("x"));
-  // define set of nuisance parameters
-  w.defineSet("nuisParams","alpha,n,a0,a1,a2,a3,a4,a5,nbkg");
-
-  mc.SetNuisanceParameters(*w.set("nuisParams"));
-
-  // import model in the workspace 
-  w.import(mc);
-
-  // write the workspace in the file
-  TString fileName = "BkPoolModel.root";
-  w.writeToFile(fileName,true);
-  cout << "model written to file " << fileName << endl; 
-
-   */
-
-  // S a v e   w o r k s p a c e   i n   f i l e
-  // -------------------------------------------
-
-  // Save the workspace into a ROOT file
+    // Save the workspace into a ROOT file
   w->writeToFile("US_workspace.root") ;
 
-
-  // Workspace will remain in memory after macro finishes
   gDirectory->Add(w) ;
 
 
