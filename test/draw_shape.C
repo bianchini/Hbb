2625a3c74339d576e8e990c816f264d1e70353a7
Lorenzo <lore.bianchini@gmail.com>
2016-08-01 10:14:28 +0200

    remove

--- test/draw_shape.C
+++ test/draw_shape.C
@@ -1,24 +1,25 @@
-{
 
-  TFile* f = TFile::Open("plots/V5/Xbb_workspace_Had_MT_MinPt100_DH1p6_MassFSR_400to1200.root");
+  {
 
-  RooWorkspace* w = (RooWorkspace*)f->Get("Xbb_workspace");
+    TFile* f = TFile::Open("plots/V7/Xbb_workspace_Had_MT_MinPt100_DH1p6_MassFSR_425to800.root");
+    
+    RooWorkspace* w = (RooWorkspace*)f->Get("Xbb_workspace");
 
-  RooAbsPdf* pdf = (RooAbsPdf*)w->pdf("buk_pdf_sgn_Spin0_M1200");
+    RooAbsPdf* pdf = (RooAbsPdf*)w->pdf("buk_pdf_sgn_Spin0_M600");
 
-  //cout << string(pdf->normRange()) << endl;
+    //cout << string(pdf->normRange()) << endl;
 
-  RooRealVar* x = (RooRealVar*)w->var("x");
+    RooRealVar* x = (RooRealVar*)w->var("x");
 
-  for(int i = 0 ; i < 100; ++i){
-    x->setVal(400+10*i);
-    cout << 400+10*i << "GeV " << pdf->getVal() << endl;;
-  }
+    for(int i = 0 ; i < 100; ++i){
+      x->setVal(400+10*i);
+      cout << 400+10*i << "GeV " << pdf->getVal() << endl;;
+    }
+    
+    pdf->setNormRange("MassFSR");
+    RooPlot* frame = x->frame(RooFit::Range(425., 800.));
+    pdf->plotOn(frame);
 
-  pdf->setNormRange("MassFSR");
-  RooPlot* frame = x->frame(RooFit::Range(400., 1200.));
-  pdf->plotOn(frame);
-
-  frame->Draw();
+    frame->Draw();
   
-}
+  }
